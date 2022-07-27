708d0bf76d78b19a2db0ec38014adc0fc8163986
João Paulo de Fáveri <joao.faveri@gmail.com>
2022-07-27 16:06:28 +0000

    ✨ feat: use nunjucks in express
    Create an MVC app.
    Use nunjucks with a template engine to render HTM views.
    Configure nunjucks.
    Set dir to static assets and base views.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 import { AppModule } from './app.module';
+import * as nunjucks from 'nunjucks';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  nunjucks.configure('views', {
+    autoescape: true,
+    express: app,
+  });
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('njk');
+
   await app.listen(3000);
 }
 bootstrap();
