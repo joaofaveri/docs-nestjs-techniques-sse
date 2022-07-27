a3ea15bede522ee64598f4a6d3587f77b0f1f3dd
João Paulo de Fáveri <joao.faveri@gmail.com>
2022-07-27 16:13:30 +0000

    ✨ feat: enable Server-Sent events on a route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Render, Sse } from '@nestjs/common';
+import { interval, map, Observable } from 'rxjs';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Render('index')
+  index() {
+    return { message: 'Testing template rendering with nunjucks' };
+  }
+
+  @Sse('sse')
+  sse(): Observable<MessageEvent> {
+    return interval(5000).pipe(
+      map(() => ({ data: 'Server-Sent Event' } as MessageEvent)),
+    );
   }
 }
